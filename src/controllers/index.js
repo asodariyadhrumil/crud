afbbaa8ee4d16b3551e936aa5d8e3e6fa3cc5e15
Dhrumil Asodariya <131768607+asodariyadhrumil@users.noreply.github.com>
2023-12-27 13:14:10 +0530

    Update index.js

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,3 +1,1 @@
 module.exports.userController=require('./user.controller')
-module.exports.productController=require('./product.controller')
-module.exports.tokenController=require('./token.controller')
