d14f50438c7846bafa6bb1b12116546652ff7a15
Dhrumil Asodariya <131768607+asodariyadhrumil@users.noreply.github.com>
2023-12-27 13:15:59 +0530

    Update index.js

--- src/validations/index.js
+++ src/validations/index.js
@@ -1,3 +1,1 @@
 module.exports.userValidation = require("./user.validation");
-module.exports.productValidation = require("./product.validation");
-module.exports.tokenValidation = require("./token.validation");
